8a92ea9c6a05c2150335148fa5cd94dea9efed7d
Agustina <agustinaayelensosa@gmail.com>
2024-06-17 17:33:48 -0300

    commit 17-6

--- my-project/src/components/Books/Books.jsx
+++ my-project/src/components/Books/Books.jsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react'
 import { getBooks } from '../../helpers/booksHelper'
 import { Link } from 'react-router-dom'
+import '../../App.css'
 
 export const Books = () => {
     const [bookState, setBookState] = useState()
@@ -18,20 +19,23 @@ export const Books = () => {
         booksGet()
     }, [])
   return (
-    <div className=' w-full min-h-screen  flex justify-evenly flex-wrap '>
-        {
-            bookState?.map((book) => {
-                return(
-                    <div key={book.index} className=' w-[300px] h-[400px] flex flex-col justify-evenly items-center bg-gray-600 radius-10 m-10 rounded-2xl shadow-black shadow-xl'>
-                        <img src={book.cover} alt="" className=' w-[200px] h-[250px]'/>
-                        <h2>{book.title}</h2>
-                        <Link to={`/books/${book.index}`}>
-                        <button >Ver mas</button>
-                        </Link>
-                    </div>
-                )
-            })
-        }
+    <div className='w-full min-h-screen flex justify-center items-center divBooks flex-wrap'>
+        
+            {
+                bookState?.map((book) => {
+                    return(
+                        <div key={book.index} className=' w-[300px] h-[400px] flex flex-col justify-evenly items-center bg-gray-600 radius-10 m-10 rounded-2xl shadow-black shadow-xl'>
+                            <img src={book.cover} alt="" className=' w-[200px] h-[250px]'/>
+                            <h2>{book.title}</h2>
+                            <Link to={`/books/${book.index}`}>
+                            <button >Ver mas</button>
+                            </Link>
+                        </div>
+                    )
+                })
+            }   
+        
+        
     </div>
   )
 }
